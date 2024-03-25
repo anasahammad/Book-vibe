5e17e424043fe9830407bb2551bfa3dc2f91bc6b
anasahammad <anasahammadsarker@gmail.com>
2024-03-25 22:27:09 +0600

    initial setup added

---
+++ src/Pages/PagesToRead.jsx
@@ -0,0 +1,10 @@
+
+const PagesToRead = () => {
+    return (
+        <div>
+            <h2 className="text-7x">Hi from Read</h2>
+        </div>
+    );
+};
+
+export default PagesToRead;
---
+++ src/main.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Root from './Root'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Pages/Home';
+import ListedBooks from './Pages/ListedBooks';
+import PagesToRead from './Pages/PagesToRead';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+
+      },
+      {
+        path: "/listed-books",
+        element: <ListedBooks></ListedBooks>
+      },
+      {
+        path: "/pages-to-read",
+        element: <PagesToRead></PagesToRead>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+)
